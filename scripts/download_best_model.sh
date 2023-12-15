d9cf7938ef97d86f2ae134b50d5ceecabfa81ec7
Ivan Dmitriev <i.u.dmitriev@gmail.com>
2023-12-15 21:54:25 +0300

    Update model download script

--- scripts/download_best_model.sh
+++ scripts/download_best_model.sh
@@ -1,6 +1,6 @@
 mkdir checkpoints
 cd checkpoints
 
-gdown https://drive.google.com/uc\?id\=1BAnj95xUmpFx1hwGx9qzwV8RBTRJ4ert
+gdown https://drive.google.com/uc\?id\=1eROIeeOfsk0iItz30y6X37EpMendKMkG
 
 cd ..
